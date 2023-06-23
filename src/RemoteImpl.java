b2cb38be764064bb6d45507300d85a92548c1fff
Eric Nicolas <ccjmne@gmail.com>
2023-06-24 00:14:12 +0200

    Fix javadoc for RemoteImpl#getAnswer

--- src/RemoteImpl.java
+++ src/RemoteImpl.java
@@ -6,11 +6,11 @@ public class RemoteImpl implements RemoteInterface {
 
   private static final Random RANDOM = new Random(System.currentTimeMillis());
 
-  @Override
   /**
    * Returns a value between 0 and 100, inclusive.
    * Returns `42` about 1/4th of the time.
    */
+  @Override
   public Integer getAnswer() {
     final Integer answer = RANDOM.nextInt(133);
     return Integer.valueOf(answer > 100 ? 42 : answer);
